1ce7a9ef189effcd352a95ee6e5fb5336a8dcaea
Augustin Anders <augustinanders@gmail.com>
2024-01-09 10:03:44 +0100

    change console logs

--- filesorter.js
+++ filesorter.js
@@ -12,26 +12,43 @@ if (!sourceFolder) {
 fs.readdir(sourceFolder, dirRead);
 
 function dirRead(err, files) {
-  console.log("Verzeichnis gelesen " + sourceFolder);
+  console.log("read directory: " + sourceFolder);
   files.forEach(processFile);
-  console.log(files);
 }
 
 function processFile(file) {
   let extension = path.extname(file).slice(1);
-  console.log("extension is " + extension);
+  let fileName = path.basename(file);
+  if (extension === "") {
+    return;
+  }
 
-  // Create the destination folder if it doesn't exist
-  fs.mkdir(path.join(sourceFolder, extension), function () {
-    console.log("Verzeichnis erstellt");
+  fs.mkdir(path.join(sourceFolder, extension), function (err) {
+    if (err) {
+      if (err.code !== "EEXIST") {
+        console.error("Error creating folder:", err);
+      }
+    } else {
+      console.log(`created folder: \x1b[1;34m${extension}\x1b[0m`);
+    }
     fs.rename(
       path.join(sourceFolder, file),
       path.join(sourceFolder, extension, file),
-      moveSuccess
+      function (err) {
+        if (err) {
+          console.error("Error moving file:", err);
+        } else {
+          moveSuccess(sourceFolder, extension, fileName);
+        }
+      }
     );
   });
 }
 
-function moveSuccess() {
-  console.log("Datei verschoben");
+function moveSuccess(sourceFolder, extension, fileName) {
+  console.log(
+    `\x1b[90mMoved \x1b[36m${fileName}\x1b[0m \x1b[30mfrom \x1b[34m${sourceFolder}\x1b[0m \x1b[30mto \x1b[34m${sourceFolder}/\x1b[0m\x1b[1;34m${extension}\x1b[0m\x1b[0m`
+  );
 }
+
+// if folder is already there, do not create it again
